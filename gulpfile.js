b63df81cf491dfcb3480f2b6ac6d23a310c7d16f
Daniel Imhoff <dwieeb@gmail.com>
2016-06-07 19:23:24 -0500

    Initial setup

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var merge = require('merge2');
+var ts = require('gulp-typescript');
+var tslint = require('gulp-tslint');
+
+gulp.task('lint', function() {
+  return gulp.src('src/**/*.ts')
+    .pipe(tslint())
+    .pipe(tslint.report("verbose"));
+});
+
+gulp.task('build', function() {
+  var tsProject = ts.createProject('tsconfig.json');
+  var tsResult = gulp.src(["src/**/*.ts"])
+    .pipe(ts(tsProject));
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest('dist')),
+    tsResult.js.pipe(gulp.dest('dist'))
+  ]);
+});
