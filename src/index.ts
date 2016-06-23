fce89e553e7d05b11f8d1cfaff0b8f3fa742be51
Daniel Imhoff <dwieeb@gmail.com>
2016-06-22 23:29:33 -0500

    Provider logic for dependency injection changes

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,75 @@
 import { Injectable, provide, Provider } from '@angular/core';
 import {
   ISettings,
-  PushOptions,
-  EventEmitter as _EventEmitter,
-  IonicCloud as _platform,
-  Push as _Push,
   Auth as _Auth,
-  User as _User,
+  AuthType,
+  Client as _Client,
+  CombinedAuthTokenContext as _CombinedAuthTokenContext,
+  Config as _Config,
+  Cordova as _Cordova,
+  Core as _Core,
   Deploy as _Deploy,
+  Device as _Device,
+  EventEmitter as _EventEmitter,
   Insights as _Insights,
-  Environment as _Environment
+  LocalStorageStrategy,
+  Logger as _Logger,
+  Push as _Push,
+  PushOptions,
+  SessionStorageStrategy,
+  SingleUserService as _SingleUserService,
+  Storage as _Storage,
+  User as _User,
+  UserContext as _UserContext
 } from '@ionic/cloud';
-
-interface Newable {
-  new(...args: any[]): any;
-}
-
-class SingletonContainer {
-  public args: any[];
-  private instance: any;
-
-  constructor(public cls: Newable, ...args: any[]) {
-    this.cls = cls;
-    this.args = args;
-  }
-
-  getInstance(): any {
-    if (!this.instance) {
-      this.instance = new this.cls(...this.args);
-    }
-
-    return this.instance;
-  }
-}
-
-@Injectable()
-export class Core {
-  init(cfg: ISettings) {
-    _platform.init(cfg);
-  }
-}
-
-@Injectable()
-export class EventEmitter extends _EventEmitter {}
-
-@Injectable()
-export class Push extends _Push {}
 
 @Injectable()
 export class Auth extends _Auth {}
 
 @Injectable()
-export class User extends _User {}
+export class Client extends _Client {}
+
+@Injectable()
+export class CombinedAuthTokenContext extends _CombinedAuthTokenContext {}
+
+@Injectable()
+export class Config extends _Config {}
+
+@Injectable()
+export class Cordova extends _Cordova {}
+
+@Injectable()
+export class Core extends _Core {}
 
 @Injectable()
 export class Deploy extends _Deploy {}
 
 @Injectable()
+export class Device extends _Device {}
+
+@Injectable()
+export class EventEmitter extends _EventEmitter {}
+
+@Injectable()
 export class Insights extends _Insights {}
 
 @Injectable()
-export class Environment extends _Environment {}
+export class Logger extends _Logger {}
+
+@Injectable()
+export class Push extends _Push {}
+
+@Injectable()
+export class SingleUserService extends _SingleUserService {}
+
+@Injectable()
+export class Storage extends _Storage {}
+
+@Injectable()
+export class User extends _User {}
+
+@Injectable()
+export class UserContext extends _UserContext {}
 
 export interface CloudSettings {
   core: ISettings;
@@ -68,19 +77,40 @@ export interface CloudSettings {
 }
 
 export function provideCloud(settings: CloudSettings): Provider[] {
-  let core = new Core();
-  core.init(settings.core);
+  let config = new Config();
+  config.register(settings.core);
 
-  let pushFactory = new SingletonContainer(Push, settings.push);
-  let deployFactory = new SingletonContainer(Deploy);
+  let emitter = new EventEmitter();
+  let logger = new Logger();
+
+  let label = 'ionic_io_auth_' + config.get('app_id');
+  let tokenContext = new CombinedAuthTokenContext(label, new LocalStorageStrategy(), new SessionStorageStrategy());
+  let client = new Client(tokenContext, config.getURL('api'));
+
+  let core = new Core(config, logger, emitter, client);
+  core.init();
+
+  let device = new Device(emitter);
+  let cordova = new Cordova({}, device, emitter, logger);
+  cordova.bootstrap();
+
+  let authModules = AuthType.createAuthModules(config, client);
 
   return [
+    provide(Auth, {'useFactory': (singleUserService: SingleUserService) => { return new Auth({}, emitter, authModules, tokenContext, singleUserService); }, 'deps': [SingleUserService]}),
+    provide(Config, {'useValue': core}),
+    provide(Cordova, {'useValue': cordova}),
     provide(Core, {'useValue': core}),
-    provide(EventEmitter, {'useValue': _platform.emitter}),
-    provide(Environment, {'useValue': new Environment()}),
-    provide(Insights, {'useValue': _platform.insights}),
-    provide(User, {'useFactory': () => { return User.current(); }}),
-    provide(Push, {'useFactory': () => { return pushFactory.getInstance() }}),
-    provide(Deploy, {'useFactory': () => { return deployFactory.getInstance() }})
+    provide(Client, {'useValue': client}),
+    provide(Deploy, {'useFactory': () => { return new Deploy({}, config, emitter, logger); }}),
+    provide(Device, {'useValue': device}),
+    provide(EventEmitter, {'useValue': emitter}),
+    provide(SingleUserService, {'useFactory': (userContext: UserContext) => { return new SingleUserService({}, client, userContext); }, 'deps': [UserContext]}),
+    provide(Insights, {'useFactory': (core: Core) => { return core.insights; }, 'deps': [Core]}),
+    provide(Logger, {'useValue': logger}),
+    provide(Push, {'useFactory': (auth: Auth, storage: Storage) => { return new Push(settings.push, config, auth, device, client, emitter, storage, logger); }, 'deps': [Auth, Storage]}),
+    provide(User, {'useFactory': (singleUserService: SingleUserService) => { return singleUserService.current(); }, 'deps': [SingleUserService]}),
+    provide(Storage, {'useFactory': () => { return new Storage({}, new LocalStorageStrategy()); }}),
+    provide(UserContext, {'useFactory': (storage: Storage) => { return new UserContext(storage, config); }, 'deps': [Storage]})
   ];
 }
