15cfe45ce13159ba0643464ec795a8f328b869f5
Daniel Imhoff <dwieeb@gmail.com>
2016-08-19 12:18:37 -0500

    Slim down injectables

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
 export * from '@ionic/cloud';
 
 import { Injectable, provide, Provider } from '@angular/core';
-import { DIContainer, ISettings } from '@ionic/cloud';
+import { DIContainer, CloudSettings } from '@ionic/cloud';
 import {
   Auth as _Auth,
-  AuthType,
   Client as _Client,
   Config as _Config,
-  Cordova as _Cordova,
-  Core as _Core,
   Deploy as _Deploy,
-  Device as _Device,
   EventEmitter as _EventEmitter,
   Insights as _Insights,
-  Logger as _Logger,
   Push as _Push,
-  SingleUserService as _SingleUserService,
   User as _User,
-  UserContext as _UserContext
 } from '@ionic/cloud';
 
 @Injectable()
@@ -30,16 +23,7 @@ export class Client extends _Client {}
 export class Config extends _Config {}
 
 @Injectable()
-export class Cordova extends _Cordova {}
-
-@Injectable()
-export class Core extends _Core {}
-
-@Injectable()
 export class Deploy extends _Deploy {}
-
-@Injectable()
-export class Device extends _Device {}
 
 @Injectable()
 export class EventEmitter extends _EventEmitter {}
@@ -48,21 +32,10 @@ export class EventEmitter extends _EventEmitter {}
 export class Insights extends _Insights {}
 
 @Injectable()
-export class Logger extends _Logger {}
-
-@Injectable()
 export class Push extends _Push {}
 
 @Injectable()
-export class SingleUserService extends _SingleUserService {}
-
-@Injectable()
 export class User extends _User {}
-
-@Injectable()
-export class UserContext extends _UserContext {}
-
-export interface CloudSettings extends ISettings {}
 
 export let container = new DIContainer();
 
@@ -78,17 +51,11 @@ export function provideCloud(settings: CloudSettings): Provider[] {
 
   return [
     provide(Auth, {'useFactory': () => { return container.auth; }}),
-    provide(Config, {'useValue': config}),
-    provide(Cordova, {'useValue': cordova}),
-    provide(Core, {'useValue': core}),
     provide(Client, {'useFactory': () => { return container.client; }}),
+    provide(Config, {'useValue': config}),
     provide(Deploy, {'useFactory': () => { return container.deploy; }}),
-    provide(Device, {'useFactory': () => { return container.device; }}),
     provide(EventEmitter, {'useFactory': () => { return container.eventEmitter; }}),
-    provide(SingleUserService, {'useFactory': () => { return container.singleUserService; }}),
-    provide(Logger, {'useFactory': () => { return container.logger; }}),
     provide(Push, {'useFactory': () => { return container.push; }}),
-    provide(User, {'useFactory': (singleUserService: SingleUserService) => { return singleUserService.current(); }, 'deps': [SingleUserService]}),
-    provide(UserContext, {'useFactory': () => { return container.userContext; }})
+    provide(User, {'useFactory': () => { return container.singleUserService.current(); }})
   ];
 }
